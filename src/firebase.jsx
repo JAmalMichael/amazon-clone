9067bb36680dfea356fc58ffd249291646d728c6
JAmal Michael <eshimi090@gmail.com>
2024-07-02 14:38:14 -1200

    firebase config

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,13 +1,20 @@
+import firebase from "firebase";
 
 
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB-GpgsqesC5iOImRfTq6UGS9wKRpRIoYQ",
-    authDomain: "clone-8ec6c.firebaseapp.com",
-    projectId: "clone-8ec6c",
-    storageBucket: "clone-8ec6c.appspot.com",
-    messagingSenderId: "580860610708",
-    appId: "1:580860610708:web:c19f4f9f9a05223f294c81",
-    measurementId: "G-4X09P1Z6ST"
-  };
+  apiKey: "AIzaSyB-GpgsqesC5iOImRfTq6UGS9wKRpRIoYQ",
+  authDomain: "clone-8ec6c.firebaseapp.com",
+  projectId: "clone-8ec6c",
+  storageBucket: "clone-8ec6c.appspot.com",
+  messagingSenderId: "580860610708",
+  appId: "1:580860610708:web:c19f4f9f9a05223f294c81",
+  measurementId: "G-4X09P1Z6ST"
+};
+
+  // Initialize Firebase
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  
+  export { db, auth };
