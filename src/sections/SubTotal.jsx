101539fed9a391efc3fcaf665edfb6a729ca5bd4
JAmal Michael <eshimi090@gmail.com>
2024-07-12 13:51:54 -1200

    currency format to number format

--- src/sections/SubTotal.jsx
+++ src/sections/SubTotal.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Styles/subtotal.css'
-import CurrencyFormat from 'react-currency-format' 
+import { NumericFormat } from 'react-number-format' 
 import { useStateValue } from '../api/StateProvider'
 import { useNavigate } from 'react-router-dom'
 import { getBasketTotal } from '../api/reducer'
@@ -13,24 +13,22 @@ function SubTotal() {
   return (
             <div className="subtotal flex flex-col justify-between w-[300px] h-[400px] p-[20px]
              bg-[#f3f3f3] border-[3px] border-[#dddddd] max-sm:w-[150px] max-sm:h-[200px]">
-            <CurrencyFormat
-            renderText={(value) => (
-                <>
-                <p>
-                    
-                    Subtotal ({basket.length} items): <strong>{value}</strong>
-                </p>
-                <small className="subtotal__gift">
-                    <input type="checkbox" /> This order contains a gift
-                </small>
-                </>
-            )}
-            decimalScale={2}
-            value={getBasketTotal(basket)} // Part of the homework
-            displayType={"text"}
-            thousandSeparator={true}
-            prefix={"$"}
-            />
+             <NumericFormat
+                    renderText={(value) => (
+                    <div>
+                        <p>
+                        Subtotal ({basket.length} items): <strong>{`$ ${value}`}</strong>
+                        </p>
+                        <small className="subtotal__gift">
+                        <input type="checkbox" /> This order contains a gift
+                        </small>
+                    </div>
+                    )}
+                    decimalScale={2}
+                    value={getBasketTotal(basket)}
+                    displayType="text"
+                    thousandSeparator=","
+           />
 
             <button className='bg-[#f0c14b] b-[2px] mt-[10px] text-[#111] h-[30px] 
             w-100%] max-sm:text-[10px]'>Proceed to Checkout</button>
